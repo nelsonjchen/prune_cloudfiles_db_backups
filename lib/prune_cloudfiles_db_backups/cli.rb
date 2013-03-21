eb35e271970bf2ed2611c4434e026aab483b9c29
Justin Force <justin.force@gmail.com>
2013-03-21 11:29:43 -0700

    Replace hash rockets

--- lib/prune_cloudfiles_db_backups/cli.rb
+++ lib/prune_cloudfiles_db_backups/cli.rb
@@ -6,9 +6,9 @@ module PruneCloudfilesDbBackups
     def self.start
       opts = Slop.parse(strict: true, help: true) do
         banner 'Usage: prune_cloudfiles_db_backups [options]'
-        on :u, :user=,      'Rackspace user to use. Default: ENV["N_RACKSPACE_API_USERNAME"]', :default => ENV['N_RACKSPACE_API_USERNAME']
-        on :k, :key=,       'Rackspace key to use. Default: ENV["N_RACKSPACE_API_KEY"]', :as => String, :default => ENV['N_RACKSPACE_API_KEY']
-        on :c, :container=, 'Rackspace key to use. Default: database_backups', :default => 'database_backups'
+        on :u, :user=,      'Rackspace user to use. Default: ENV["N_RACKSPACE_API_USERNAME"]', default: ENV['N_RACKSPACE_API_USERNAME']
+        on :k, :key=,       'Rackspace key to use. Default: ENV["N_RACKSPACE_API_KEY"]', as: String, default: ENV['N_RACKSPACE_API_KEY']
+        on :c, :container=, 'Rackspace key to use. Default: database_backups', default: 'database_backups'
         on :y, :yes,        'WARNING: Actually delete files from Rackspace Cloud. Without this option, only a listing of files to be deleted are given'
       end
 
