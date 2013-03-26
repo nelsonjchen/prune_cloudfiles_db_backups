d2659c2ac5d6bc4ee861c903a29dea4e568b2d5a
Nelson Chen <crazysim@gmail.com>
2013-03-26 16:18:19 -0700

    write out spec for backup

--- spec/prune_cloudfiles_db_backups/backup_spec.rb
+++ spec/prune_cloudfiles_db_backups/backup_spec.rb
@@ -1,9 +1,28 @@
 require "rspec"
 require 'prune_cloudfiles_db_backups/backup'
 
-
 module PruneCloudfilesDbBackups
   describe Backup do
+    describe '#initialize' do
+      it 'can create a backup with a name, date, and set of object names'
+
+      it 'can create a new backup with no flags set'
+    end
+
+    describe '#deletable?' do
+      it 'returns false if either the name, date, or set of object are not set'
+      it 'returns true if none of the daily, weekly, or monthly flags are set'
+    end
+
+    describe '#date' do
+      it 'allows reading of the date'
+    end
+
+    %w{daily weekly monthly}.map do |interval|
+      describe "##{interval}" do
+        it "allows setting of the #{interval} flag"
+      end
+    end
 
   end
 end
