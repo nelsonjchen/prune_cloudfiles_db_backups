0e3da70123f55156446f4c4835911c3eed75cb11
Nelson Chen <crazysim@gmail.com>
2013-03-26 16:30:28 -0700

    Specced out names

--- lib/prune_cloudfiles_db_backups/backup.rb
+++ lib/prune_cloudfiles_db_backups/backup.rb
@@ -1,6 +1,6 @@
 module PruneCloudfilesDbBackups
   class Backup
-    def intialize(args)
+    def intialize(*args)
 
     end
   end
--- spec/prune_cloudfiles_db_backups/backup_spec.rb
+++ spec/prune_cloudfiles_db_backups/backup_spec.rb
@@ -4,14 +4,27 @@ require 'prune_cloudfiles_db_backups/backup'
 module PruneCloudfilesDbBackups
   describe Backup do
     describe '#initialize' do
-      it 'can create a backup with a name, date, and set of object names'
+      before(:each) do
+        @names = %w{
+          reports_production-20120819000003.pgdump
+          reports_production-20120819000003.pgdump.000
+          reports_production-20120819000003.pgdump.001
+        }
+      end
+      it 'can create a backup with a set of object names' do
+        Backup.new(names:@names)
+      end
 
-      it 'can create a new backup with no flags set'
+      it 'can create a new backup with no flags set' do
+        b = Backup.new(names:@names)
+        b.daily || b.weekly || b.monthly == false
+      end
     end
 
     describe '#deletable?' do
       it 'returns false if either the name, date, or set of object are not set'
       it 'returns true if none of the daily, weekly, or monthly flags are set'
+      it 'returns false if no flags are set'
     end
 
     describe '#date' do
