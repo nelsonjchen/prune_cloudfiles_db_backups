78086821eb728204c6d930c7332a32bed77cba5f
Nelson Chen <crazysim@gmail.com>
2013-03-27 13:42:45 -0700

    retention calculator spec

--- spec/prune_cloudfiles_db_backups/cli_spec.rb
+++ spec/prune_cloudfiles_db_backups/cli_spec.rb
@@ -2,9 +2,6 @@ require 'spec_helper'
 require 'prune_cloudfiles_db_backups/cli'
 
 module PruneCloudfilesDbBackups
-
   describe CLI do
-    it 'is a command line'
-    it 'creates a RetentionCalculator'
   end
 end
--- spec/prune_cloudfiles_db_backups/retention_calculator_spec.rb
+++ spec/prune_cloudfiles_db_backups/retention_calculator_spec.rb
@@ -4,5 +4,21 @@ require 'prune_cloudfiles_db_backups/retention_calculator'
 module PruneCloudfilesDbBackups
 
   describe RetentionCalculator do
+    context 'March 24, 2013' do
+      it 'is able to be created on this date with a stubbed Time#now'
+
+      it 'accepts keyword initialization arguments'
+
+      it 'accepts an Array of Strings'
+
+      describe '#to_delete' do
+
+      end
+
+      describe '#to_keep' do
+
+      end
+    end
+
   end
 end
