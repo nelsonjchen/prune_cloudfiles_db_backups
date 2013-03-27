c4428a96fea71330a10ff441853aca9f314a09bd
Nelson Chen <crazysim@gmail.com>
2013-03-26 17:09:27 -0700

    try to parse the date

--- lib/prune_cloudfiles_db_backups/backup.rb
+++ lib/prune_cloudfiles_db_backups/backup.rb
@@ -1,6 +1,10 @@
+require 'date'
+
 module PruneCloudfilesDbBackups
   class Backup
     attr_accessor :daily, :weekly, :monthly
+    attr_reader :date
+
     # Creates a new instance of backup with a grouped list of related files
     # @param [Array] array
     # @return [Backup]
@@ -8,6 +12,8 @@ module PruneCloudfilesDbBackups
       @daily = false
       @weekly = false
       @monthly = false
+      str_date = /.*(\d{14}).*/.match(array[:names].first)[0]
+      @date = DateTime.strptime(str_date, '%Y%m%d%H%M%S')
     end
 
     def deletable?
