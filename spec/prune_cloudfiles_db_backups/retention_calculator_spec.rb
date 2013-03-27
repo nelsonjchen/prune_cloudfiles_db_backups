e01134388e14a3729cdf24b57ef0a0e5f607c1f1
Nelson Chen <crazysim@gmail.com>
2013-03-27 13:48:23 -0700

    stub out time#now

--- spec/prune_cloudfiles_db_backups/retention_calculator_spec.rb
+++ spec/prune_cloudfiles_db_backups/retention_calculator_spec.rb
@@ -5,6 +5,11 @@ module PruneCloudfilesDbBackups
 
   describe RetentionCalculator do
     context 'March 24, 2013' do
+      before(:each) do
+        @time_now = Time.parse(self.class.description)
+        Time.stub!(:now).and_return(@time_now)
+      end
+
       it 'is able to be created on this date with a stubbed Time#now'
 
       it 'accepts keyword initialization arguments'
