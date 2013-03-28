545a435abf6c20b5d0bc2d6e2e307dea66cb1d9b
Nelson Chen <crazysim@gmail.com>
2013-03-28 13:45:13 -0700

    make nicer initialization

--- lib/prune_cloudfiles_db_backups/retention_calculator.rb
+++ lib/prune_cloudfiles_db_backups/retention_calculator.rb
@@ -2,12 +2,21 @@ require 'active_support/time'
 
 module PruneCloudfilesDbBackups
   class RetentionCalculator
-    DAY_RETENTION   = 14
-    WEEK_RETENTION  =  8
-    MONTH_RETENTION = 12
 
+    # Initialize the calculator with a list of cloudfile
+    # container objects
     # @param [Array] objects from cloudfiles
-    def initialize(objects)
+    # @param [Integer] day_retention
+    # @param [Integer] week_retention
+    # @param [Integer] month_retention
+    def initialize( objects,
+                    day_retention = 14,
+                    week_retention = 8,
+                    month_retention = 12)
+      @day_retention = day_retention
+      @week_retention = week_retention
+      @month_retention = month_retention
+
       @now = DateTime.now
 
       sets = objects.group_by do |o|
@@ -22,27 +31,27 @@ module PruneCloudfilesDbBackups
         weekly = keep_weekly_dates.include?(datetime.to_date)
         monthly = keep_monthly_dates.include?(datetime.to_date)
 
-        Backup.new(objects:objects, datetime:datetime, daily:daily, weekly: weekly, monthly:monthly)
+        Backup.new(objects: objects, datetime: datetime, daily: daily, weekly: weekly, monthly: monthly)
       end
 
     end
 
     # Based off of http://www.infi.nl/blog/view/id/23/Backup_retention_script
     def keep_daily_dates
-      @daily_dates ||= (0...DAY_RETENTION).map do |i|
+      @daily_dates ||= (0...@day_retention).map do |i|
         @now.at_midnight.advance(days: -i).to_date
       end
     end
 
     def keep_weekly_dates
-      @weekly_dates ||= (0...WEEK_RETENTION).map do |i|
+      @weekly_dates ||= (0...@week_retention).map do |i|
         # Sunday on the last couple of weeks
         @now.at_beginning_of_week(:sunday).advance(weeks: -i).to_date
       end
     end
 
     def keep_monthly_dates
-      @monthly_dates ||= (0...MONTH_RETENTION).map do |i|
+      @monthly_dates ||= (0...@month_retention).map do |i|
         # First Sunday of every month
         @now.at_beginning_of_month.advance(months: -i).advance(days: 6).beginning_of_week(:sunday).to_date
       end
