879971b6138b60fbb89c67ddb6c9dcfd0757b61d
Nelson Chen <crazysim@gmail.com>
2013-03-28 15:22:08 -0700

    strip down pruner for rspec development

--- lib/prune_cloudfiles_db_backups/pruner.rb
+++ lib/prune_cloudfiles_db_backups/pruner.rb
@@ -1,32 +1,18 @@
-require 'cloudfiles'
+require 'openstack'
 require 'prune_cloudfiles_db_backups/retention_calculator'
 
 module PruneCloudfilesDbBackups
   class Pruner
     def initialize(opts)
-      cf = CloudFiles::Connection.new(username: opts[:user], api_key: opts[:key])
+      cf = OpenStack::Connection.new(username: opts[:user],
+                                      api_key: opts[:key],
+                                      auth_url: 'https://identity.api.rackspacecloud.com/v1.0',
+                                      service_type:'object-store')
       container = cf.container(opts[:container])
 
       # Based off of http://www.infi.nl/blog/view/id/23/Backup_retention_script
-      results = RetentionCalculator.new(container.list_objects).results
-
-      results.keep_objects.map do |o|
-        puts "Keeping: #{o}"
-      end
-
-      if opts[:yes]
-        results.delete_objects.map do |o|
-          puts "Deleting: #{o}"
-          # Not until I'm comfortable
-          #container.delete_object(o)
-        end
-      else
-        results.delete_objects.map do |o|
-          puts "To be deleted: #{o}"
-        end
-      end
-
-      puts "Count: TBD - #{results.delete_objects.size} Keep - #{results.keep_objects.size}"
+      calc = RetentionCalculator.new(container.list_objects)
     end
+
   end
 end
